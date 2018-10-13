6363fd8cb8829e30e5754cb4b3d8d533ec23fa08
TTRSQ <tst.ttrsq.123@gmail.com>
2018-10-13 06:07:43 +0000

    手数料を加味してコインの数量がずれないよう調整

--- up.py
+++ up.py
@@ -17,6 +17,12 @@ class Exchange:
         self.balance = {}
         self.last_order = {}
 
+    def get_commission(self):
+        self.exchange.get_commission()
+
+    def my_commission(self):
+        return self.exchange.my_commission()
+
     def get_board(self):
         command = 'board'
         self.board = self.exchange.format(command, self.exchange.hit_api(command))
@@ -24,6 +30,9 @@ class Exchange:
     def get_balance(self):
         command = 'balance'
         self.balance = self.exchange.format(command, self.exchange.hit_api(command))
+
+    def round_order_size(self, size):
+        return self.exchange.round_order_size(size)
 
     def order(self, param):
         command = 'order'
@@ -38,9 +47,6 @@ class Exchange:
         command = 'order_list'
         print(self.exchange.hit_api(command))
 
-    def get_permission(self):
-        command = 'permissions'
-        print(self.exchange.hit_api(command))
 
 api_config = {
     'method' : {
@@ -55,6 +61,7 @@ api_config = {
 
 # 固有取引所カセットが継承するクラス
 class ExchangeCharacterBase:
+    commission = 0
 
     def __init__(self, default):
         self.url_public  = default['url_public']
@@ -79,6 +86,14 @@ class ExchangeCharacterBase:
             f.write(json.dumps(response_dic)+"\n")
             f.close()
         return response_dic
+
+    # 取引所ごとに扱える最小桁数が違う
+    def round_order_size(self, size):
+        return size
+
+    # 手数料の取得(あればオーバーライド)
+    def get_commission(self):
+        ExchangeCharacterBase.commission = 0
 
     # 関数の返値を成形
     @staticmethod
@@ -121,8 +136,8 @@ class BitFlyer(ExchangeCharacterBase):
                     'method' : api_config['method']['post'],
                     'type'   : api_config['type']['private']
                 },
-                'permissions' : {
-                    'path'   : '/v1/me/getpermissions',
+                'commission' : {
+                    'path'   : '/v1/me/gettradingcommission?product_code=BTC_JPY',
                     'method' : api_config['method']['get'],
                     'type'   : api_config['type']['private']
                 }
@@ -140,7 +155,6 @@ class BitFlyer(ExchangeCharacterBase):
         }
         url = self.url_public + self.api_list[command]['path']
         method = 'GET' if self.api_list[command]['method'] == api_config['method']['get'] else 'POST'
-
         if self.api_list[command]['type'] == api_config['type']['private']:
             header['ACCESS-KEY'] = self.api_key
             unix_stamp = str(int(datetime.now().timestamp()))
@@ -154,6 +168,13 @@ class BitFlyer(ExchangeCharacterBase):
             return urllib.request.Request(url, method=method, headers=header, data=json.dumps(param).encode('utf-8'))
         else:
             return urllib.request.Request(url, method=method, headers=header)
+
+    def get_commission(self):
+        res = self.hit_api('commission')
+        BitFlyer.commission = res['commission_rate']
+
+    def my_commission(self):
+        return BitFlyer.commission
 
     def format(self, command, values):
         return self.formatter[command](values)
@@ -169,6 +190,7 @@ class BitFlyer(ExchangeCharacterBase):
     def format_order(self, values):
         ret_value = {
             'success'    : 1 if 'child_order_acceptance_id' else 0,
+            'retry'      : 0,
             'created_at' : int(datetime.now().timestamp()*1000)
         }
         return ret_value
@@ -177,6 +199,15 @@ class BitFlyer(ExchangeCharacterBase):
         return self.prepare_dic[command](params)
 
     def prepare_order(self, params):
+        # 手数料があるので購入は多く、売却は少なく
+        if params['side'] == 'buy':
+            params['size'] /= (1-BitFlyer.commission)
+        else:
+            params['size'] /= (1+BitFlyer.commission)
+
+        round = 100000000
+        params['size'] = ( 1.0*int(params['size']*round) )/round
+
         if 'price' in params:
             return {
                 "product_code": "BTC_JPY",
@@ -272,6 +303,9 @@ class BitBank(ExchangeCharacterBase):
         else:
             return urllib.request.Request(url, method=method, headers=header)
 
+    def my_commission(self):
+        return BitBank.commission
+
     def format(self, command, values):
         return self.formatter[command](values)
 
@@ -291,13 +325,25 @@ class BitBank(ExchangeCharacterBase):
         return ret_value
 
     def format_order(self, values):
-        ret_value = {
-            'side'       : values['data']['side'],
-            'size'       : values['data']['start_amount'],
-            'created_at' : values['data']['ordered_at'],
-            'success'    : values['success'],
-        }
-        return ret_value
+        if values['success']:
+            ret_value = {
+                'side'       : values['data']['side'],
+                'size'       : values['data']['start_amount'],
+                'created_at' : values['data']['ordered_at'],
+                'success'    : values['success'],
+            }
+            return ret_value
+        else:
+            if values['data']['code'] == 70009:
+                return {"success": 0 , "retry": 1}
+            else:
+                return {"success": 0 , "retry": 0}
+
+    # bitbankは小数点以下4桁まで 以降切り捨て
+    def round_order_size(self, size):
+        # 丸め込みをする(小数点以下8桁)
+        round = 10000
+        return ( 1.0*int(size*round) )/round
 
 
     def prepare(self, command, params):
@@ -367,6 +413,8 @@ class Trader:
         self.ex1.get_balance()
         self.th0 = threading.Thread()
         self.th1 = threading.Thread()
+        self.th00 = threading.Thread()
+        self.th11 = threading.Thread()
         self.threshold = {'size': 0.001, 'price' : 1000, 'bias': 500}
         self.cart = 0
         self.f = open("/home/tatsuya/tmp/log", "a")
@@ -378,6 +426,10 @@ class Trader:
         print(self.ex0.exchange.name, self.ex0.balance)
         print(self.ex1.exchange.name, self.ex1.balance)
 
+    def disp_commission(self):
+        print(self.ex0.exchange.name, self.ex0.my_commission())
+        print(self.ex1.exchange.name, self.ex1.my_commission())
+
     def update_balance(self, exchange):
         exchange.get_balance()
 
@@ -388,7 +440,7 @@ class Trader:
         self.board_is_new = 0
 
     def order(self, exchange, side, size, exec):
-        now = str(int(datetime.now().timestamp()))
+        now = (datetime.now() + timedelta(hours=9)).strftime("%Y/%m/%d %H:%M:%S")
         if size < self.threshold['size']:
             return
         if exec:
@@ -396,7 +448,20 @@ class Trader:
                 'size' : size,
                 'side' : side
             })
-            self.f.write(now + exchange.exchange.name + side + str(size) + '\n')
+
+            self.f.write(now + ' ' + exchange.exchange.name + ' ' + side + ' ' + str(size) + '\n')
+            price = self.get_mean_value_from_size(size ,exchange)
+            bid_ask = 'bid' if side == 'sell' else 'ask'
+            self.f.write(exchange.exchange.name + ' expect price:'+ str(price[bid_ask]) + '\n')
+
+            # 指値でリトライ
+            if exchange.last_order['success'] == 0 and exchange.last_order['retry']:
+                exchange.order({
+                    'size'  : size,
+                    'price' : (price[bid_ask]*1.1 if side == 'buy' else price[bid_ask]*0.9),
+                    'side'  : side
+                })
+
         else:
             self.f.write(now + exchange.exchange.name + side + str(size) + '\n')
 
@@ -475,37 +540,44 @@ class Trader:
 
         return size
 
+    def get_commission(self):
+        self.ex0.get_commission()
+        self.ex1.get_commission()
 
     # dir = 1(ex0 -> ex1),2はその逆
     def trade(self, dir, size, exec):
+        # より強く丸めた方を採用
+        round_size0 = self.ex0.round_order_size(size)
+        round_size1 = self.ex1.round_order_size(size)
+        size = min(round_size0, round_size1)
         if dir == 1:
-            self.th0 = threading.Thread(name="ex0", target=self.order, args=(self.ex0, 'buy' , size, exec, ))
-            self.th1 = threading.Thread(name="ex1", target=self.order, args=(self.ex1, 'sell', size, exec, ))
+            self.th00 = threading.Thread(name="ex0", target=self.order, args=(self.ex0, 'buy' , size, exec, ))
+            self.th11 = threading.Thread(name="ex1", target=self.order, args=(self.ex1, 'sell', size, exec, ))
         else:
-            self.th0 = threading.Thread(name="ex0", target=self.order, args=(self.ex0, 'sell', size, exec, ))
-            self.th1 = threading.Thread(name="ex1", target=self.order, args=(self.ex1, 'buy' , size, exec, ))
-        self.th0.start()
-        self.th1.start()
+            self.th00 = threading.Thread(name="ex0", target=self.order, args=(self.ex0, 'sell', size, exec, ))
+            self.th11 = threading.Thread(name="ex1", target=self.order, args=(self.ex1, 'buy' , size, exec, ))
+        self.th00.start()
+        self.th11.start()
 
     def shopping(self, exchange, exec):
         self.update_board(exchange)
         self.cart += 1
         if self.cart == 2:
             self.cart = 0
-            now = str(int(datetime.now().timestamp()))
+            now = (datetime.now() + timedelta(hours=9)).strftime("%Y/%m/%d %H:%M:%S")
             mes = self.max_effective_size(self.threshold['price'], self.threshold['bias'], 1.0)
             if mes[1] > mes[2]:
                 if mes[1] > self.threshold['size']:
                     mta = self.max_trade_amount(mes[1], 1.0)
                     self.f.write('\n'+now+' dir1 '+str(self.ex1.board['bids'][0]['price']-self.ex0.board['asks'][0]['price'])+'\n')
-                    self.f.write('size: {mta:'+str(mta[1]*0.8)+', mes:'+str(mes[1]*0.8)+'}\n')
-                    self.trade(1, min(mta[1]*0.8, mes[1]*0.8), exec)
+                    self.f.write('size: {mta:'+str(mta[1]*0.5)+', mes:'+str(mes[1]*0.8)+'}\n\n')
+                    self.trade(1, min(mta[1]*0.5, mes[1]*0.8), exec)
             else:
                 if mes[2] > self.threshold['size']:
                     mta = self.max_trade_amount(mes[2], 1.0)
                     self.f.write('\n'+now+' dir2 '+str(self.ex0.board['bids'][0]['price']-self.ex1.board['asks'][0]['price'])+'\n')
-                    self.f.write('size: {mta:'+str(mta[2]*0.8)+', mes:'+str(mes[2]*0.8)+'}\n')
-                    self.trade(2, min(mta[2]*0.8, mes[2]*0.8), exec)
+                    self.f.write('size: {mta:'+str(mta[2]*0.5)+', mes:'+str(mes[2]*0.8)+'}\n\n')
+                    self.trade(2, min(mta[2]*0.5, mes[2]*0.8), exec)
 
 
     def parallel_shopping(self, exec):
@@ -514,6 +586,11 @@ class Trader:
         self.th0.start()
         self.th1.start()
 
+trader = Trader(Exchange(BitFlyer()), Exchange(BitBank()))
+trader.get_commission()
+trader = None
+
+time.sleep(2.0)
 
 for i in range(1800*24*2):
     now = datetime.now() + timedelta(hours=9)
@@ -524,8 +601,5 @@ for i in range(1800*24*2):
         trader = Trader(Exchange(BitFlyer()), Exchange(BitBank()))
         trader.parallel_shopping(1)
     time.sleep(2.0)
-
-
-
 
 exit()
